2f215b7bb914afc58bca8ea9ccb5f9a362dd04d3
jwalker-WZ <jwalker07@live.com>
2015-04-29 00:56:00 -0500

    Added Database stuff, basic command string

--- HerdManager/DatabaseStuff/DataBaseHelper.cs
+++ HerdManager/DatabaseStuff/DataBaseHelper.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data.Common;
 
 namespace HerdManager.DatabaseStuff
 {
     public static class DataBaseHelper
     {
+        static string connString = "Server=rei.cs.ndsu.nodak.edu;Database=csci366_hmanager;Uid=csci366_hmanager;Pwd=*****;";
+        MySqlConnection conn;
+        MySqlCommand comm;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+
+
     }
 }
